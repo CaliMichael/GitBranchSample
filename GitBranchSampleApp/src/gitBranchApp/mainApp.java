cb1e51e2c472b7d03740df82faa4254d03f845e4
CaliMichael <tem11226@gmail.com>
2021-06-08 16:10:33 +0800

    branch

--- GitBranchSampleApp/src/gitBranchApp/mainApp.java
+++ GitBranchSampleApp/src/gitBranchApp/mainApp.java
@@ -5,6 +5,18 @@ public class mainApp {
 		//this is the 1st version
 		System.out.println("1st version");
 		
+		Math cMath= new Math();
+		System.out.println();
+		System.out.println(cMath.add(10, 20));
+		
+		
 	}
 
+
+}
+
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
